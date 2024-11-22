2fb262cd3f07163f8c84b55900fd5bf917bf1833
Skiby7 <l.scoppitto@studenti.unipi.it>
2024-11-22 18:32:01 +0100

    mock harness, more endpoints

--- src/tux_service/service/libs/db_mocks.py
+++ src/tux_service/service/libs/db_mocks.py
@@ -4,9 +4,7 @@ def use_mocks(func):
     def wrapper(*args, **kwargs):
         if os.getenv("TEST_RUN", "false").lower() == "true":
             mock_func = getattr(f"{func.__name__}_mock", locals()[__name__], default_mock)
-            print(f"Function {func.__name__} is mocked.")
             return mock_func(*args, **kwargs)
-        print(f"Function {func.__name__} is executed normally.")
         return func(*args, **kwargs)
     return wrapper
 
