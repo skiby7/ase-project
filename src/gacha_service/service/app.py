b973b9e04565c08445bc8615a4b9e04d955385a3
StangaSimo <simostanga@libero.it>
2024-11-23 12:23:33 +0100

    add test,all endpoint

--- src/gacha_service/service/app.py
+++ src/gacha_service/service/app.py
@@ -1,54 +1,54 @@
-#from flask import Flask, request, make_response, jsonify
 from fastapi import FastAPI, Query, HTTPException
 from fastapi.responses import JSONResponse
-#import random, time, os, threading, requests
-#import json
-from db import database
+from database.db import database
+import uuid 
 
-#DB initialization 
-#app = Flask(__name__)
-#db = database("distros.txt")
-
-#TODO: check for admin and user (?)
-#This function return all the gachas in the system 
-#@app.route('/all')
-#def all():
-#   return make_response(db.get_all_distros(),200) 
-
-#TODO: check user 
-#@app.route('/roll')
-#def roll():
-#   return make_response(NULL,200) 
-#
-##TODO: check user 
-#@app.route('/modify')
-#def modify():
-#   return make_response(NULL,200) 
-
-#testing porpuses
-#@app.route('/add')
-#def add():
-#    a = request.args.get('a', type=float)
-#    b = request.args.get('b', type=float)
-#    if a and b:
-#        return make_response(jsonify(s=a+b), 200) 
-#    else:
-#        return make_response('Invalid input\n', 400) 
-#
-#if __name__ == '__main__':
-#    app.run(debug=True)
+#TODO: non moccare db, moccare tux, doppio docker compose
+#TODO: change path to /admin /user
+#TODO: check user  
+#TODO: jwt sub (uuid), role
 
 
 # Inizializzazione DB
 app = FastAPI()
-db = database("distros.txt")
+db = database("utils/distros.json")
 
-# Funzione per restituire tutti i gachas nel sistema
-@app.get("/all", status_code=200)
-def get_all():
-    return JSONResponse(content=db.get_all_distros())
+# View system gacha collection
+@app.get("/user/gacha/all", status_code=200)
+def user_gacha():
+    return JSONResponse(content=db.get_all_distros_names())
 
-# Funzione di aggiunta per test
+# TODO: View user personal gacha collection
+@app.get("/user/gacha/personal", status_code=200)
+def user_gacha():
+    return 
+
+# TODO: View Specific Gacha Info
+@app.get("/user/gacha/specific", status_code=200)
+def user_gacha():
+    return 
+
+# TODO: Use In-Game Currency to Roll Gach
+@app.get("/user/gacha/roll", status_code=200)
+def user_gacha():
+    return
+
+# TODO: Admin View Gacha Collection
+@app.get("/admin/gacha/collection", status_code=200)
+def user_gacha():
+    return
+
+# TODO: Admin Modify Gacha Collection, insert or delete new gacha
+@app.get("/admin/gacha/modify", status_code=200)
+def user_gacha():
+    return
+
+# TODO: Admin Modify Specific Gacha
+@app.get("/admin/gacha/modify/specific", status_code=200)
+def user_gacha():
+    return
+
+# testing porpuses
 @app.get("/add", status_code=200)
 def add(a: int, b: int):
     if a is not int and b is not int:
---
+++ src/gacha_service/service/database/db.py
@@ -0,0 +1,84 @@
+from pymongo import MongoClient
+import json
+import uuid
+
+#TODO user on db
+class database:
+    def __init__(self,distrofile):
+        self.distrofile = distrofile
+        self.client = MongoClient("db", 27017, maxPoolSize=50)
+        self.db = self.client["mydatabase"]
+        if "gachas" in self.db.list_collection_names():
+            print(" \n\n SONO VIVO  \n\n")
+        else: 
+            self.db_inizialization()
+        if "users" in self.db.list_collection_names():
+            print(" \n\n falso  \n\n")
+        else: 
+            self.db.create_collection("users")
+
+    # convert to json distro.txt
+    def db_inizialization(self):
+        self.db.create_collection("gachas")
+        gachas = self.db["gachas"]
+
+        with open(self.distrofile, "r") as file:
+            data = json.load(file)
+
+        for gacha in data:
+            gacha["id"] = str(uuid.uuid4())
+
+        gachas.insert_many(data)
+
+    def get_all_distros_names(self):
+        gachas = self.db["gachas"]
+        all_distros = list(gachas.find())
+        all = [{"name": gachas["name"]} for gachas in all_distros]
+        return all
+
+    def add_user_gacha(self,user_id,gacha_name):
+        gachas = self.db["gachas"]
+        users = self.db["users"]
+        gacha = gachas.find_one({"name": gacha_name})
+
+        if not gacha:
+            raise ValueError(f"Gacha with name '{gacha_name}' not found.")
+
+        gacha_id = gacha["id"]
+        user = users.find_one({"id": user_id})
+
+        if not user:
+            user = {
+                "id": user_id,
+                "gacha_list": [
+                                {gacha_id : 1}
+                ]
+            }
+            users.insert_one(user)
+        else : 
+            existing_gacha = next((item for item in user["gacha_list"] if item["gacha_id"] == gacha_id), None)
+    
+            if existing_gacha:
+                users.update_one(
+                    {"id": user_id, "gacha_list.gacha_id": gacha_id},
+                    {"$inc": {"gacha_list.$.value": 1}}
+                )
+            else:
+                users.update_one(
+                    {"id": user_id},
+                    {"$push": {"gacha_list": {"gacha_id": gacha_id, "value": 1}}}
+                )
+
+    def get_user_gacha(self,user_id):
+        gachas = self.db["gachas"]
+        users = self.db["users"]
+        user = users.find_one({"id": user_id})
+        user_gachas = list(user["gacha_list"])
+        return user_gachas 
+    
+
+
+
+
+
+
