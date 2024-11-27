b1bac53cd281b461340708cd061e4a6b783c76eb
Luca Rizzo <lucalucasrizzo@gmail.com>
2024-11-27 11:58:50 +0100

    Https on mongodb

--- src/authentication_service/service/utils/mongo_connection.py
+++ src/authentication_service/service/utils/mongo_connection.py
@@ -1,19 +1,25 @@
-from typing import Annotated
-from fastapi import Depends
 from pymongo import MongoClient
 from pymongo.collection import Collection
 
-mongo_client: MongoClient = None
+mongo_client: MongoClient | None = None
+
+username = "root"
+port = "27017"
+auth_source = "admin"
+database = "my_database"
 
 def startup_db_client(container_name = "db_authentication"):
+    with open('/run/secrets/mongodb_password', 'r') as file:
+        password = file.read().strip()
     global mongo_client
-    mongo_client = MongoClient(f"mongodb://{container_name}:27017")
+    uri = f"mongodb://{username}:{password}@{container_name}:{port}/{database}?authSource={auth_source}&tls=true&tlsAllowInvalidCertificates=true"
+    mongo_client = MongoClient(uri)
 
 def get_db_client():
     return mongo_client
 
 def get_accounts_collection() -> Collection:
-    db = mongo_client["mydatabase"]
+    db = mongo_client[database]
     return db["accounts"]
 
 def delete_accounts_collection():
