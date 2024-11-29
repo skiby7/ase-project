5090f390a8abf155f530c9308c9618990623baac
Skiby7 <l.scoppitto@studenti.unipi.it>
2024-11-29 19:02:27 +0100

    removed mocks

--- src/tux_service/service/libs/mocks.py
+++ src/tux_service/service/libs/mocks.py
@@ -1,22 +1,10 @@
 import os
 from logging import getLogger
-import random
 import sys
 from functools import wraps
-from routers.transactions.models import PurchaseTransactionModel
-from uuid import uuid4
 from time import time
 logger = getLogger("uvicorn.error")
 unix_time = lambda: int(time())
-class MockSession:
-    def open(self):
-        pass
-    def close(self):
-        pass
-    def __enter__(self):
-        pass
-    def __exit__(self, exception_type, exception_value, exception_traceback):
-        pass
 
 
 def use_mocks(func):
@@ -36,86 +24,3 @@ def default_mock(*args, **kwargs):
 def verify_mock(token: str) -> bool:
     logger.debug(f"MOCK -> Verifying token {token}")
     return True
-
-
-def buy_tux_mock(*args, **kwargs):
-    pass
-
-def create_purchase_transaction_mock(*args, **kwargs):
-    pass
-
-
-def create_tables_mock(*args, **kwargs):
-    pass
-
-
-def create_user_balance_mock(*args, **kwargs):
-    pass
-
-
-def create_user_transaction_mock(*args, **kwargs):
-    pass
-
-
-def delete_user_account_mock(*args, **kwargs):
-    pass
-
-
-def get_transaction_by_id_mock(*args, **kwargs):
-    amount = random.random()*random.randint(0, 1000)
-    return PurchaseTransactionModel(
-                    transaction_id=str(uuid4()),
-                    amount_fiat=amount,
-                    amount_tux=amount*1.2,
-                    timestamp=unix_time(),
-                    user_id=str(uuid4()),
-                    filled=random.random() > 0.80)
-
-
-
-def get_user_fiat_balance_mock(*args, **kwargs):
-    pass
-
-
-def get_user_transactions_mock(*args, **kwargs):
-    pass
-
-
-def get_user_tux_balance_mock(*args, **kwargs):
-    pass
-
-
-def settle_auction_payments_mock(*args, **kwargs):
-    pass
-
-
-def to_set_mock(*args, **kwargs):
-    pass
-
-
-def transactional_mock(*args, **kwargs):
-    pass
-
-
-def unix_time_mock(*args, **kwargs):
-    pass
-
-
-def update_freezed_tux_mock(*args, **kwargs):
-    pass
-
-
-def update_game_balance_mock(*args, **kwargs):
-    pass
-
-
-def update_user_tux_balance_mock(*args, **kwargs):
-    pass
-
-
-def use_mocks_mock(*args, **kwargs):
-    pass
-
-
-def wraps_mock(*args, **kwargs):
-    pass
