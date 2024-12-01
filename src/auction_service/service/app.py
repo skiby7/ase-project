7b50331c86fcdf6333010062fb78a0bf1d892754
Filippo Fornai <filippofornai1@gmail.com>
2024-12-01 13:41:21 +0100

    Added auth with jwt.

--- src/auction_service/service/app.py
+++ src/auction_service/service/app.py
@@ -1,24 +1,20 @@
-import uvicorn
 from database.db import database
-import uuid
 from uuid import UUID
-from logging import getLogger
-from pydantic import BaseModel
-from fastapi import Body, FastAPI, HTTPException
-from fastapi.encoders import jsonable_encoder
+from fastapi import Body, FastAPI, HTTPException, Depends
 import time
 from apscheduler.schedulers.background import BackgroundScheduler
 from utils.util_classes import Auction,Bid,AuctionOptional,BidOptional,IdStrings
-import shared_libs.access_token_utils
+from utils.check import check_admin,check_tux,check_user
+from auth.access_token_utils import extract_access_token
+from auth.access_token_utils import TokenData
+from typing import Annotated
+
 
 unix_time = lambda: int(time.time())
 
 ### INIT ###
 
-mock_authentication = None
-mock_tux = None
-mock_distro = None
-
+mock_check = None
 
 #dummy_player_id = "123e4567-e89b-12d3-a456-426614174000"
 
@@ -34,7 +30,7 @@ def checkAuctionExpiration():
     print("SCHEDULED FUNCTION",flush=True)
     finishedAuctions = db.checkAuctionExpiration()
     for auction in finishedAuctions:
-        if not mock_distro:break
+        if not mock_check:break
         #url_api
         #requests.get(url_api)
         #give gacha to winning player
@@ -43,13 +39,6 @@ def checkAuctionExpiration():
 scheduler = BackgroundScheduler()
 scheduler.add_job(checkAuctionExpiration,"interval",minutes=CHECK_EXPIRY_INTERVAL)
 scheduler.start()
-
-def check_user(user): #0 USER, 1 ADMIN
-    if mock_authentication==True:return True
-    if True: #da cambiare
-        return True
-    else: 
-        raise HTTPException(status_code=400, detail="Invalid Admin")
 
 
 # HP for every endpoint: ENDPOINT_NAME - HP IN WHICH IT WORKS
@@ -64,35 +53,35 @@ def check_user(user): #0 USER, 1 ADMIN
 
 # DONE
 # AUCTION_CREATE - player_id == auction.player_id
-@app.post("/{player_id}/auction/auction-create", status_code=201)
-def player_endpoint(player_id:UUID,auction:Auction):
-    check_user(0)
+@app.post("/auction/{player_id}/auction-create", status_code=201)
+def player_endpoint(player_id:UUID,auction:Auction,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
 
-    if player_id != auction.player_id:
-        raise HTTPException(status_code=400, detail="Cannot create other player's auctions. Set own player_id")
+    if (player_id != auction.player_id) or (player_id != token_data.sub):
+        raise HTTPException(status_code=400, detail="Player_id not valid.")
 
-    db.auction_create(auction,mock_distro)
+    db.auction_create(auction,mock_check)
     
 
 # DONE
 # AUCTION_DELETE - playerd_id is owner of auction_id
-@app.delete("/{player_id}/auction/auction-delete", status_code=200)
-def player_endpoint(player_id:UUID,auction_id:UUID):
-    check_user(0)
+@app.delete("/auction/{player_id}/auction-delete", status_code=200)
+def player_endpoint(player_id:UUID,auction_id:UUID,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
 
-    if player_id != db.auction_owner(auction_id):
-        raise HTTPException(status_code=400, detail="Player is not the owner of the specified auction.")
-    db.auction_delete(auction_id,False,mock_distro,mock_tux)
+    if player_id != db.auction_owner(auction_id) or (player_id != token_data.sub):
+        raise HTTPException(status_code=400, detail="Player_id not valid.")
+    db.auction_delete(auction_id,False,mock_check)
 
 
 # DONE
 # AUCTION_FILTER - "active":True
 @app.get("/auction/auction-filter", status_code=200)
-def player_endpoint(auction_filter:AuctionOptional):
-    check_user(0)
+def player_endpoint(auction_filter:AuctionOptional,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
     
     if auction_filter.active is not None and auction_filter.active is False:
-        raise HTTPException(status_code=400, detail="active field must be set to True for player to use this endpoint")
+        raise HTTPException(status_code=400, detail="Active field must be set to True for player to use this endpoint")
         
     return db.auction_filter()
 
@@ -102,25 +91,25 @@ def player_endpoint(auction_filter:AuctionOptional):
 
 # DONE
 # AUCTION_BID - player_id == bid.player_id
-@app.post("/{player_id}/auction/bid", status_code=201)
-def player_endpoint(player_id:UUID,bid:Bid):
-    check_user(0)
+@app.post("/auction/{player_id}/bid", status_code=201)
+def player_endpoint(player_id:UUID,bid:Bid,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
 
-    if player_id != bid.player_id:
-        raise HTTPException(status_code=400, detail="Cannot bid for other players. Set own player_id")
+    if player_id != bid.player_id or (player_id != token_data.sub):
+        raise HTTPException(status_code=400, detail="Player_id not valid.")
 
     #extract player id
-    db.bid(bid,mock_tux)
+    db.bid(bid,mock_check)
 
 
 # DONE
 # BID_FILTER - player_id == bid_filter.player_id
-@app.get("/{player_id}/auction/bid-filter", status_code=200)
-def player_endpoint(player_id:UUID,bid_filter:BidOptional):
-    check_user(0)
+@app.get("/auction/{player_id}/bid-filter", status_code=200)
+def player_endpoint(player_id:UUID,bid_filter:BidOptional,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
 
-    if player_id != bid_filter.player_id:
-        raise HTTPException(status_code=400, detail="Cannot access other player bids data. Set own player_id")
+    if player_id != bid_filter.player_id or (player_id != token_data.sub):
+        raise HTTPException(status_code=400, detail="Player_id not valid.")
     
     #extract player id
     return db.bid_filter(player_id)
@@ -134,40 +123,30 @@ def player_endpoint(player_id:UUID,bid_filter:BidOptional):
 ##### AUCTION #####
 
 
-# DONE
+# TODO Check if the player exists, maybe is done if gacha is available
 # AUCTION_CREATE
-@app.post("/admin/auction/auction-create", status_code=201)
-def player_endpoint(auction:Auction):
-    check_user(1)
+@app.post("/auction/admin/auction-create", status_code=201)
+def player_endpoint(auction:Auction,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
 
-    db.auction_create(auction,mock_distro)
+    db.auction_create(auction,mock_check)
 
-'''
-# DONE
-# AUCTION_MODIFY
-@app.patch("/admin/auction/auction-modify", status_code=200)
-def admin_endpoint(auction_id:UUID,auction_modifier:AuctionOptional):
-    check_user(1)
-    
-    db.auction_owner(auction_id)
-    db.auction_modify(auction_id,auction_modifier)
-'''
 
 # DONE
 # AUCTION_DELETE
-@app.delete("/admin/auction/auction-delete", status_code=200)
-def admin_endpoint(auction_id:UUID):
-    check_user(1)
+@app.delete("/auction/admin/auction-delete", status_code=200)
+def admin_endpoint(auction_id:UUID,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
     
     db.auction_owner(auction_id)
-    db.auction_delete(auction_id,mock_distro,mock_tux)
+    db.auction_delete(auction_id,mock_check)
 
 
 #DONE
 # AUCTION_FILTER
-@app.get("/admin/auction/auction-filter", status_code=200)
-def player_endpoint(auction_filter:AuctionOptional):
-    check_user(1)
+@app.get("/auction/admin/auction-filter", status_code=200)
+def player_endpoint(auction_filter:AuctionOptional,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
 
     return db.auction_filter(auction_filter)
 
@@ -175,44 +154,35 @@ def player_endpoint(auction_filter:AuctionOptional):
 ##### BID #####
 
 
-#DONE
+# TODO Controllare che il player specificato nel bid esista
 # BID_CREATE
-@app.post("/admin/auction/bid", status_code=201)
-def player_endpoint(bid: Bid):
-    check_user(1)
+@app.post("/auction/admin/bid", status_code=201)
+def player_endpoint(bid: Bid,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
 
-    db.bid(bid,mock_tux)
+    db.bid(bid,mock_check)
 
-'''
-# BID_MODIFY
-@app.patch("/admin/auction/bid-modify", status_code=200)
-def admin_endpoint(bid_id:UUID,bid_modifier:BidOptional):
-    check_user(1)
-    
-    db.bid_owner(bid_id)
-    db.bid_modify(bid_id,bid_modifier)
-'''
 
 # BID_DELETE
-@app.delete("/admin/auction/bid-delete", status_code=201)
-def admin_endpoint(bid_id:UUID):
-    check_user(0)
+@app.delete("/auction/admin/bid-delete", status_code=201)
+def admin_endpoint(bid_id:UUID,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_user(mock_check,token_data)
 
     db.bid_owner(bid_id)
     db.bid_delete(bid_id)
 
 
 # BID_FILTER
-@app.get("/admin/auction/bid-filter", status_code=200)
-def player_endpoint(bid_filter:BidOptional):
-    check_user(1)
+@app.get("/auction/admin/bid-filter", status_code=200)
+def player_endpoint(bid_filter:BidOptional,token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
 
     return db.bid_filter(bid_filter)
 
 
 # MARKET_ACTIVITY
-@app.get("/admin/auction/market-activity", status_code=200)
-def admin_endpoint():
-    check_user(1)
+@app.get("/auction/admin/market-activity", status_code=200)
+def admin_endpoint(token_data: Annotated[TokenData, Depends(extract_access_token)]):
+    check_admin(mock_check,token_data)
 
     return db.market_activity()
--- src/auction_service/service/app_test.py
+++ src/auction_service/service/app_test.py
@@ -3,7 +3,4 @@ import app as main_app
 tested_app = main_app.app
 
 # User 1
-main_app.mock_authentication = True 
-main_app.mock_distro = True
-main_app.mock_tux = True
-main_app.mock_player_id = True
+main_app.mock_check = True
