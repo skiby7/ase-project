f197b1c939eb64b20899dbdb66203e6e3bbd786e
Leonardo Scoppitto <l.scoppitto@zerynth.com>
2024-12-02 11:58:38 +0100

    auth in transaction and delete idempotent1

--- src/tux_service/service/routers/transactions/transactions.py
+++ src/tux_service/service/routers/transactions/transactions.py
@@ -1,12 +1,14 @@
 from libs.auth import verify
 from libs.db.db import  get_db, get_user_auction_transactions, get_user_purchase_transactions, get_user_roll_transactions, user_exists
-from fastapi import APIRouter, HTTPException, Header, Depends
+from fastapi import APIRouter, HTTPException, Depends
+from libs.access_token_utils import TokenData, extract_access_token
+from typing import Annotated
 
 router = APIRouter()
 
 @router.get("/transactions/{user_id}")
-def transactions(user_id: str, Authorization: str = Header(), db_session = Depends(get_db)):
-    if not verify(Authorization):
+def transactions(token_data: Annotated[TokenData, Depends(extract_access_token)], user_id: str, db_session = Depends(get_db)):
+    if not verify(token_data, user_id, False):
         raise HTTPException(status_code=401, detail="Unauthorized")
     if not user_exists(db_session, user_id):
         raise HTTPException(status_code=404, detail="User does not exists!")
