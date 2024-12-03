2bbcf82489656d3930ef78eace282d2d1affa0ee
Skiby7 <l.scoppitto@studenti.unipi.it>
2024-12-03 18:13:11 +0100

    verify

--- src/tux_service/service/libs/auth.py
+++ src/tux_service/service/libs/auth.py
@@ -10,5 +10,6 @@ def verify(token: TokenData, user_id: Optional[str], admin: bool) -> bool:
     role_is_admin = token.role == 'admin'
     if admin:
         return role_is_admin
-
+    if role_is_admin:
+        return True
     return token.sub == user_id
