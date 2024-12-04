0ba8919f7a19db061c808fee4c7c85914edaf8e1
Simone Stanganini <simostanga@libero.it>
2024-12-04 15:18:16 +0100

    modify regex gacha_name

--- src/gacha_service/service/gacha_utils/gacha.py
+++ src/gacha_service/service/gacha_utils/gacha.py
@@ -10,7 +10,7 @@ def verify_name(name: str) -> bool:
     if len(name) > 30:
         return False
 
-    if re.fullmatch(r"[a-zA-Z!?_]*", name):
+    if re.fullmatch(r"[a-zA-Z!?_ ]*", name):
         return True
     else:
         return False
