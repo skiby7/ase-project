e25c7cd7e12cbad79e2b309c59153bb431f6a79a
Luca Rizzo <lucalucasrizzo@gmail.com>
2024-12-04 12:36:56 +0100

    integration tests workflow (to correct)

---
+++ src/integration_tests/run_tests.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker compose -f docker-compose-integration-test.yml up --build --abort-on-container-exit --exit-code-from newman
+docker compose -f docker-compose-integration-test.yml down
