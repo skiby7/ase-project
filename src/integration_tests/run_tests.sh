42da8c01b4bc4f6b5c8c9df4d3d81803019d205f
Leonardo Scoppitto <l.scoppitto@zerynth.com>
2024-12-04 13:59:31 +0100

    integration test

--- src/integration_tests/run_tests.sh
+++ src/integration_tests/run_tests.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+
+set -e
+
 docker compose -f docker-compose-integration-test.yml up --build --abort-on-container-exit --exit-code-from newman
 docker compose -f docker-compose-integration-test.yml down
