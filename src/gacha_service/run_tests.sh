796eccb3b95c1cd6878e3b1de27328f10aae422c
Leonardo Scoppitto <l.scoppitto@zerynth.com>
2024-12-05 12:19:34 +0100

    run_test.sh

--- src/gacha_service/run_tests.sh
+++ src/gacha_service/run_tests.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+
+cleanup() {
+    docker compose -f docker-compose-test.yml down
+}
+
+trap cleanup EXIT
 docker compose -f docker-compose-test.yml up --build --abort-on-container-exit --exit-code-from newman
